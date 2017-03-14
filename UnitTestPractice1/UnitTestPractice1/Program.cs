29c6cb863a0f344a0235ad00e6f0d25d4a39b42f
fernarvaez6 <fernarvaez_6@yahoo.com>
2017-03-13 20:04:47 -0700

    Add files via upload

---
+++ UnitTestPractice1/UnitTestPractice1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestPractice1
+{
+        public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Hash("leepadg"));
+            Console.ReadLine();
+
+        }
+
+        public static Int64 Hash(String s)
+        {
+            //variables
+            Int64 h = 7;
+            String letters = "acdegilmnoprstuw";
+            int index = 0;
+            Int64 inc = 0; //Int64 por que no se puede multiplicar long * int64
+
+                //ciclo FOR
+                for (int k =0; s.Length > k; k++)
+                {
+                inc = h * 37;
+                index = letters.IndexOf(s[k]);
+
+                h = inc + index;
+            
+                }
+                return h;
+        }
+    }
+}
---
+++ UnitTestPractice1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestPractice1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Int64 result = 680131659347;
+
+            String text = "leepadg";
+
+            Assert.AreEqual(result, Program.Hash(text));
+
+
+
+            /*
+             * 
+            function Hash( s : string) : int64
+            begin
+            h : int64;
+            letters : string;
+            i : int;
+            index : int;
+            inc : int; 
+
+            h = 7;
+                letters = “acdegilmnoprstuw”;
+
+            for ( i from 0 to length(s) )
+            begin
+            inc = h * 37;
+            index = indexOf s[i] in letters;
+            h = inc + index;
+            end
+
+            return h;
+            end
+                         */
+
+        }
+    }
+}
